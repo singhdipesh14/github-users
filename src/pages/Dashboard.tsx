72d5f3e16b17315ff5b52e39cc4e5a039f590c12
singhdipesh14 <58405253+singhdipesh14@users.noreply.github.com>
2021-09-21 00:02:49 +0530

    transfer complete

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { Info, Repos, User, Search, Navbar } from "../components"
+import loadingImage from "../images/preloader.gif"
+import { GithubContext } from "../context/context"
+
+const Dashoard = () => {
+	return (
+		<main>
+			<h2>Dashboard Page</h2>
+		</main>
+	)
+}
+
+export default Dashoard
